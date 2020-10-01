2c1d4c62db37eb9af48015ab85e41831abbf8f6f
CecileNavez <cecile.navez@bnpparibasfortis.com>
2020-10-01 10:40:35 +0200

    ExB3

---
+++ ExB4/src/test/java/be/abis/exa1/ut/TestPerson.java
@@ -0,0 +1,45 @@
+package be.abis.exa1.ut;
+
+import be.abis.exa1.exception.PersonShouldBeAdultException;
+import be.abis.exa1.model.Person;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TestPerson {
+
+    private Person person;
+    private Person person2;
+
+    @Before
+    public void setUp() {
+        person = new Person(1, "Cecile", "Navez", LocalDate.of(1978, 5, 16));
+        person2 = new Person(1, "Nathan", "Dupont", LocalDate.of(2003, 5, 16));
+    }
+    
+
+    @Test
+    public void ageOfPersonFromBirthDateShouldBe42() throws PersonShouldBeAdultException{
+
+        int personAge = person.calculateAge();
+        assertThat(personAge, is(equalTo(42)));
+        }
+
+    @Test
+    public void toStringSentenceStartsWithPerson() {
+
+        String strPerson = person.toString();
+        assertThat(strPerson, startsWith("Person"));
+    }
+
+    @Test(expected = PersonShouldBeAdultException.class)
+    public void ageOfThePersonShouldBeOver18() throws PersonShouldBeAdultException {
+        int personAge = person2.calculateAge();
+    }
+}
---
+++ ExB4/src/test/java/be/abis/exa1/ut/TestSuite.java
@@ -0,0 +1,13 @@
+package be.abis.exa1.ut;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        TestAddress.class, TestPerson.class
+})
+
+public class TestSuite {
+}
